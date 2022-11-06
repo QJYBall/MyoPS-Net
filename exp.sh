5823e201cd535481f156e4f6c35408cbda4df4d0
BallBallQ <24294232+QJYBall@users.noreply.github.com>
2022-11-06 15:34:02 +0800

    Delete exp.sh

--- exp.sh
+++
@@ -1,2 +0,0 @@
-nohup python main.py --batch_size 16 --dim 192 --lr 1e-4 --threshold 0.50 --end_epoch 200 &
-# python predict.py --load_path checkpoints --predict_mode multiple --threshold 0.50 --dim 192
